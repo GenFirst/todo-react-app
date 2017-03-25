b9a2f7215480cf25eeac7addfd91e0c07f6a8394
Ivan Vasiljevic <robince885@gmail.com>
2017-03-26 00:02:08 +0100

    Add blueprint of application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, NavItem, Nav, Col, Grid, Row, FormGroup, FormControl, Checkbox, Button, ButtonGroup } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  constructor()
+  {
+    super();
+    this.todos = ['1', '2', '3'];
+  }
+
+  getTodos(todo) {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <li>
+        <Checkbox checked />
+        <lable>{todo}</lable>
+        <Button>Remove</Button>
+      </li>
+    );
+  }
+
+  render() {
+    let newTodos = this.todos.map(todo => this.getTodos(todo));
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">TODOs Application</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">TODOs</NavItem>
+              <NavItem eventKey={2} href="#">About</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <form>
+          <Grid>
+            <Row>
+              <Col xs={12}>
+                <h1 className="text-center">TODOs tasks</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} xsOffset={3}>
+                <FormGroup controlId="formNewTodo">
+                  <FormControl
+                    type="text"
+                    placeholder="What needs to be done?"
+                  />
+                </FormGroup>
+
+                {newTodos}
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} xsOffset={3}>
+                <Col xs={3}><p>{this.todos.length} items left</p></Col>
+                <Col xs={9}>
+                  <ButtonGroup>
+                    <Button>All</Button>
+                    <Button>Active</Button>
+                    <Button>Completed</Button>
+                  </ButtonGroup>
+                </Col>
+              </Col>
+            </Row>
+          </Grid>
+        </form>
       </div>
     );
   }
